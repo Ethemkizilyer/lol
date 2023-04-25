f7ade46be7306db2a23fce275fe1a22cffdb0b69
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-04-26 01:33:54 +0300

    images added

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -5,11 +5,11 @@ import { addFavorite, getCards, removeFavorite, setCards } from '../features'
 import { Link } from 'react-router-dom'
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import Buttons from './Buttons'
+import { Tooltip } from '@mui/material'
 
 const Cards = () => {
     const dispatch=useDispatch<AppDispatch>()
     const cards= useSelector((state:AppStore)=>state.cards)
-    const favorites = useSelector((store: AppStore) => store.favorites);
     const [isIntersecting, setIsIntersecting] = useState(false);
 
 
@@ -30,10 +30,26 @@ const Cards = () => {
         >
           <div>
             <Link to={`/cards/${item.id}`}>
-              <img src={item.image} alt={item.image} />
+              <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${item.id}_0.jpg`} alt={item.image} />
             </Link>
             <Buttons id={item.id}/>
-       
+       <div>
+        <div>{item.id}</div>
+        <div>{item.title}</div>
+        <div>
+            {item.tags.map(tag=>{
+                return (
+                  <Tooltip title={`${tag}`} arrow key={`${tag}`}>
+                    <img
+                      src={`/tags/${tag}.png`}
+                      alt={`${item.id + tag}`}
+                      key={`${item.id + tag}`}
+                    />
+                  </Tooltip>
+                );
+            })}
+        </div>
+       </div>
           </div>
         </li>
       ))}
