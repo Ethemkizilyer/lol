76cc4cf49275c2877f558b85b2442b0b50a6a87c
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-04-26 14:50:29 +0300

    react-tilty library added

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom'
 import Buttons from './Buttons'
 import { Tooltip } from '@mui/material'
 import "../styles/Cards.scss"
+import { motion, useAnimation } from "framer-motion";
+import Tilty from 'react-tilty'
 const Cards = () => {
     const dispatch=useDispatch<AppDispatch>()
     const cards= useSelector((state:AppStore)=>state.cards)
@@ -19,14 +21,42 @@ const Cards = () => {
     }, [])
 
       
-    
+     const variants = {
+       visible: { x: 0 },
+       hidden: { x: -100 },
+     };
 
+       const animation = useAnimation();
+
+       const list = {
+         hidden: { opacity: 1 },
+         visible: {
+           transition: {
+             staggerChildren: "infinite",
+           },
+         },
+       };
+       const item = {
+         visible: {
+           x: 0,
+           opacity: 1,
+         },
+         hidden: {
+           x: "-500px",
+           opacity: 0,
+           height: "50px",
+           width: "50px",
+           y: "500px",
+         },
+       };
   return (
     <div className="cards">
+    
       {cards?.map((item, i) => (
+        <Tilty>
         <li
           key={item.id}
-          className={`card_card ${isIntersecting ? "show" : "hidden"}`}
+          // className={`card_card ${isIntersecting ? "show" : "hidden"}`}
         >
           <div className="card_card_img">
             <Link to={`/cards/${item.id}`} className="card_card_link">
@@ -54,7 +84,7 @@ const Cards = () => {
               </div>
             </div>
           </div>
-        </li>
+        </li></Tilty>
       ))}
     </div>
   );
