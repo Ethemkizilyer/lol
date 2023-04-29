11d25fbefd1582c59031e0f6128b9719741a6559
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-04-30 00:10:03 +0300

    updated

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,17 +1,19 @@
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-import { AppStore } from "../store";
+import { AppDispatch, AppStore } from "../store";
 import { useDispatch, useSelector } from "react-redux";
 import { addFavorite, removeFavorite } from "../features";
+import { ICards } from "../types/cards.types";
 
 const Buttons = ({ id }: { id: string }) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   const favorites = useSelector((store: AppStore) => store.favorites);
   const cards = useSelector((store: AppStore) => store.cards);
 
-  const findFav = favorites?.find((fav: any) => fav.id === id);
-  const filterPerson = cards?.find((p:any) => p.id === id);
+  const findFav = favorites?.find((fav: ICards) => fav.id === id);
+  const filterPerson = cards?.find((p: ICards) => p.id === id);
 
-  function manageFavorites() {
+  const manageFavorites=():void=> {
+    console.log("object")
     findFav
       ? dispatch(removeFavorite(filterPerson))
       : dispatch(addFavorite(filterPerson));
@@ -19,7 +21,7 @@ const Buttons = ({ id }: { id: string }) => {
   return (
     <button aria-label="add to favorites" onClick={() => manageFavorites()}>
       {!findFav ? (
-        <AiOutlineHeart style={{ color: "red" }} className="fav-icon asd" />
+        <AiOutlineHeart style={{ color: "red" }} className="fav-icon " />
       ) : (
         <AiFillHeart className="fav-icon" style={{ color: "red" }} />
       )}
